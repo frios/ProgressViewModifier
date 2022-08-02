56e74e30eedf450be97699d1c11077af45e44d1b
Ferdinand Rios <frios@viciouscircle.com>
2022-08-02 13:19:28 -0700

    added background

--- Sources/ProgressViewModifier/ProgressViewModifier.swift
+++ Sources/ProgressViewModifier/ProgressViewModifier.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ProgressViewModifier: ViewModifier {
     let color: Color
+    let bkColor: Color?
     
     func body(content: Content) -> some View {
         content
@@ -10,11 +11,12 @@ struct ProgressViewModifier: ViewModifier {
             .foregroundColor(color)
             .scaleEffect(1.5, anchor: .center)
             .progressViewStyle(CircularProgressViewStyle(tint: color))
+            .background(RoundedRectangle(cornerRadius: 5.0).foregroundColor(bkColor))
     }
 }
 
 extension ProgressView {
-    public func progressStyle(color: Color) -> some View {
-        modifier(ProgressViewModifier(color: color))
+    public func progressStyle(color: Color, bkColor: Color? = nil) -> some View {
+        modifier(ProgressViewModifier(color: color, bkColor: bkColor))
     }
 }
