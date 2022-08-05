5083dbf9b62c45fa76f9acb0a9b646db8db5adb8
Ferdinand Rios <frios@viciouscircle.com>
2022-08-05 11:35:45 -0700

    more experimenting

--- Sources/ProgressViewModifier/ProgressViewModifier.swift
+++ Sources/ProgressViewModifier/ProgressViewModifier.swift
@@ -10,15 +10,15 @@ struct ProgressViewModifier: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .frame(width: width, height: height)
+//            .frame(width: width, height: height)
+            .background(RoundedRectangle(cornerRadius: 20.0)
+                .foregroundColor(bkColor)
+                .frame(width: width, height: height))
             .font(.system(size: fontSize))
             .lineLimit(2)
             .foregroundColor(color)
             .scaleEffect(1.0, anchor: .center)
             .progressViewStyle(CircularProgressViewStyle(tint: color))
-            .background(RoundedRectangle(cornerRadius: 20.0)
-                .background(bkColor)
-                .frame(width: width, height: height))
     }
 }
 
